517c4c9d1a3e81e8466aa52b2ef727d8554276f7
cpauvert <charlie.pauvert@etu.univ-rouen.fr>
2015-09-05 13:39:44 +0200

    Intégration M0 dans la fonction d'estimation des paramètres + fonctions de convenance

--- fonctions.R
+++ fonctions.R
@@ -2,13 +2,36 @@ require(seqinr)
 
 #' Fonction de calcul de l'estimation de la matrice de transition
 #' d'une chaîne de Markov d'ordre $k$.
-
 estimMarkovK<-function(fasta, ordre, alphabet ) {
-  #ordre=1
-  tabCount<-count(fasta[-length(fasta)], ordre)
-  # attention à ne pas prendre en compte la dernière lettre
-  tabCountPlus<-count(fasta, ordre+1)
-  Nij<-matrix(tabCountPlus, byrow = T, ncol=length(alphabet), dimnames = list( alphabet, names(tabCountPlus)))
-  matT<-Nij/as.vector(tabCount)
+  Nij<-matrix(count(fasta, ordre+1), byrow = T, ncol=length(alphabet))
+  
+  if(ordre != 0){
+    Ni<-count(fasta[1: (length(fasta)-ordre)], ordre)
+  }
+  else {
+    Ni<-length(fasta)
+  }
+  matT<-Nij/as.vector(Ni)
   return(matT)
 }
+
+LogVraisCorr<-function(matTrans,fasta, ordre, alphabet) {
+  # log vraisemblance corrigée pour éviter log(0) = -Inf
+  # si on a pas toutes les transitions 
+  
+  # Matrice des transitions observées dans la séquence
+  NAlphaBeta<-matrix(count(fasta, ordre+1), byrow = T, ncol=length(alphabet))
+  return(sum(NAlphaBeta*(log1p(matTrans))))
+}
+
+rapportLogVrais<-function(L0, L1) {
+  # Fonction de convenance
+  return(-2*(L0-L1))
+}
+
+# Fonctions de convenances
+AIC<-function(L0, K) -2*L0+2*K
+BIC<-function(L0, K, n) -2*L0+K*log(n)
+
+
+
