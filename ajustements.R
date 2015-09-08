ce7fb52017d92afbf210b865a0eb12ca65f1765d
cpauvert <charlie.pauvert@etu.univ-rouen.fr>
2015-09-08 21:10:43 +0200

    Finalisation et commentaires des fonctions d'ajustements.

--- ajustements.R
+++ ajustements.R
@@ -7,7 +7,7 @@ source("fonctions.R")
 
 
 
-ajustementRapportVrais<-function(mMax, fasta, alphabet){
+ajustementRapportVrais<-function(mMax, fasta, alphabet, seuil=0.05){
   # En prenant le test de rapport de
   #  vraisemblance comme critère d'ajustement, 
   #  on cherche l'ordre de chaîne de Markov
@@ -19,7 +19,7 @@ ajustementRapportVrais<-function(mMax, fasta, alphabet){
   Trace<-data.frame(matrix(nrow = mMax+1, 
                            ncol = 1, 
                            dimnames=list(0:mMax,"pvalue")))
-  print(Trace)
+#   print(Trace)
   m<-0
   repeat { 
     L0<-LogVraisCorr(matTrans = estimMarkovK(fasta, m, alphabet),
@@ -30,55 +30,111 @@ ajustementRapportVrais<-function(mMax, fasta, alphabet){
                       fasta = fasta,
                       ordre = m+1,
                       alphabet)
+    
+    # Calcul pvalue pour test du rapport de vraisemblance :
+    # H0: modèle de markov ordre m
+    # H1: modèle de markov ordre m+1
+    ##################################
+    # /!\ le Trace[m+1] correspond à l'ordre m (artifice de numérotation R)
+    ##################################
     Trace[m+1, 1]<-rapportLogVrais(alphabet, ordre = m, L0, L1)
     
-   if(m+1 == mMax){ break }
+    if(is.finite(Trace[m+1,1]) ) {
+      # Test si le rapport est NaN.
+      
+#       cat(paste0("DBG: m=",m,"  trace[m+1]=",Trace[m+1,1],"\n"))
+      if ( (m == mMax) ){
+        # On arrête l'ajustement lorsque l'on a
+        #  atteint l'itération maximale, ou
+        #  que l'on a pas incrémenter l'ordre m
+        cat(paste0("Itération max. Ordre de la chaîne : ",m,"\n"))
+        break
+      }
+      if( Trace[m+1,1] > seuil ){
+        # On arrête l'ajustement lorsque l'on a
+        #  l'on rejette H1. (Pvalue > seuil)
+        
+        # On conclut donc à l'ordre m si m = 0.
+        #  et m-1 sinon.
+        if ( m == 0){
+          cat(paste0("Ordre chaîne Markov ajusté ",m,"\n"))
+        }  else {
+          cat(paste0("Ordre chaîne Markov ajusté ",m-1,"\n"))
+        }
+        break
+      }
+    } else{
+      # On arrête l'ajustement si la valeur calculée est NaN
+      cat(paste0("NaN calculée\nOrdre chaîne Markov ajusté ",m-1,"\n"))
+      break
+    }
+    
    m<-m+1
   }
-  # On arrête l'ajustement lorsque l'on rejette le modèle de complexité supérieure
-  #  ou que l'on a atteint l'itération maximale.
     
-  cat(paste0("Ordre chaîne Markov ajustée :",m," \n", sep=" "))
   return(Trace)
 }
 
 # ajustementRapportVrais(mMax = 4, fasta = s2c(seqTest), ALPH)
 
-ajustementAIC<-function(mMax, fasta, alphabet, eps=0.1){
-  # En prenant l'AIC comme critère d'ajustement, 
-  #  on cherche l'ordre de chaîne de Markov
-  #  pour lequel on atteint un limite de mMax
+ajustementCritere<-function(critere, mMax, fasta, alphabet, eps=0.1){
+  # En prenant critère d'ajustement à minimiser (AIC/BIC par ex), 
+  #  on cherche l'ordre de chaîne de Markov optimal
+  #  dans la limite de mMax itérations.
   #
   
   # Tableau trace 
   # R commence à 0; et m+1 nécessaire pour comparer
   Trace<-data.frame(matrix(nrow = mMax+1, 
                            ncol = 1, 
-                           dimnames=list(0:mMax,"AIC")))
+                           dimnames=list(0:mMax,critere)))
   m<-0
   repeat { 
     L0<-LogVraisCorr(matTrans = estimMarkovK(fasta, m, alphabet),
                       fasta = fasta,
                       ordre = m,
                       alphabet)
+    # Estimation (grossière) du nombre de paramètres (non optimisé)
     Nparametre<-length(alphabet)*length(alphabet)^m
-    Trace[m+1, 1]<-AIC(L0, K = Nparametre)
     
-   if(is.finite(Trace[m+1,1])) {
-     if ( (m == mMax) || (Trace[m+1,1] < Trace[m,1])){     
-      break
+    # Valeur du critère pour l'ordre m
+    Trace[m+1, 1]<-switch(critere,
+                          "AIC" = AIC(L0, K = Nparametre),
+                          "BIC" = BIC(L0, K = Nparametre,n=length(fasta)))
+    
+    # Sauvegarde du critère précédent
+    #  initialisé à Inf pour m = 0.
+    critere.old<-ifelse(m == 0,Inf, Trace[m,1] )
+    
+    cat(paste0("m: ",m,"  LL: ",L0,"  Trace:", Trace[m+1,1]," OLD: ", critere.old,"\n"))
+   if(is.finite(Trace[m+1,1]) ) {
+     if ( m == mMax ) {
+       # On arrête l'ajustement lorsque 
+       #  si l'on a atteint l'itération maximale. 
+       cat(paste0("Itération max. Ordre de la chaîne avec ", critere," : ",m,"\n"))
+       break
      }
-   } 
-   else{
-     print("NaN Error !")
+      if ( Trace[m+1,1] > critere.old ){
+        cat(paste0("Ordre chaîne Markov ajusté avec ",critere," : ",m-1,"\n"))
+        break
+      }
+   } else{
+     # On arrête l'ajustement si la valeur calculée est NaN
+     cat(paste0("NaN calculée\nOrdre chaîne Markov ajustée ",critere," : ",m-1,"\n"))
      break
    }
     
    m<-m+1
   }
-  # On arrête l'ajustement lorsque l'on a atteint l'itération maximale.
-  cat(paste0("Ordre chaîne Markov ajustée AIC :",m," \n", sep=" "))
+  
   return(Trace)
 }
 
-ajustementAIC(mMax = 4, fasta = s2c(seqTest),ALPH)
+ajustementCritere("AIC",mMax = 4, fasta = s2c(seqTest),ALPH)
+ajustementCritere("BIC",mMax = 4, fasta = s2c(seqTest),ALPH)
+
+
+pfuRapport<-ajustementRapportVrais(mMax = 7, fasta = pfu,alphabet = ALPH)
+pfuAIC<-ajustementCritere("AIC",mMax = 7, fasta = pfu,ALPH)
+pfuBIC<-ajustementCritere("BIC",mMax = 7, fasta = pfu,ALPH)
+
